41d669989d82506da471b8a1fbc1f56dcad4259a
KaushikSarveswaran <Kaushik3497@yahoo.co.in>
2019-07-23 23:59:10 +0530

    Removed the subfolders outside GAN

--- prostate/train_local.sh
+++
@@ -1,5 +0,0 @@
-BATCH_SIZE=16
-EXP_DIR=/media/htic/NewVolume5/miccai_mri/models/localdisc_prostate_github/
-TRAIN_PATH=/media/htic/NewVolume3/Balamurali/promise_prostate_dataset/train
-VALID_PATH=/media/htic/NewVolume3/Balamurali/promise_prostate_dataset/test
-sudo /home/htic/anaconda2/envs/torch4/bin/python -W ignore train_local.py --exp-dir $EXP_DIR  --batch-size $BATCH_SIZE --train-path $TRAIN_PATH --validation-path $VALID_PATH
--- prostate/train_local_dyn.sh
+++
@@ -1,5 +0,0 @@
-BATCH_SIZE=1
-EXP_DIR=/media/htic/NewVolume5/miccai_mri/models/localdyndisc_prostate_github/
-TRAIN_PATH=/media/htic/NewVolume3/Balamurali/promise_prostate_dataset/train_dyn_16
-VALID_PATH=/media/htic/NewVolume3/Balamurali/promise_prostate_dataset/test
-sudo /home/htic/anaconda2/envs/torch4/bin/python -W ignore train_local_dyn.py --exp-dir $EXP_DIR  --batch-size $BATCH_SIZE --train-path $TRAIN_PATH --validation-path $VALID_PATH
